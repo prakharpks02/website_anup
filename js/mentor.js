4ff1fc27520e1371168719a012476b3aca8eeacb
Anup1410 <anupr1410@gmail.com>
2020-05-14 17:06:07 +0530

    added know more page

---
+++ js/mentor.js
@@ -0,0 +1,26 @@
+function on_sandeep() {
+    document.getElementById("sandeep_overlay").style.display = "block";
+  }
+  
+  function off_sandeep() {
+    document.getElementById("sandeep_overlay").style.display = "none";
+  }
+
+
+
+  function on_love() {
+    document.getElementById("love_overlay").style.display = "block";
+  }
+  
+  function off_love() {
+    document.getElementById("love_overlay").style.display = "none";
+  }
+
+
+  function on_ashirwad() {
+    document.getElementById("ashirwad_overlay").style.display = "block";
+  }
+  
+  function off_ashirwad() {
+    document.getElementById("ashirwad_overlay").style.display = "none";
+  }
