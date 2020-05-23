b7d5a24c467772f24289e10cfa2f371928c3c968
Anup1410 <anupr1410@gmail.com>
2020-05-23 10:22:59 +0530

    mistaked cleared

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,22 @@ $('.menu-toggle').click(function(){
    $(".nav").toggleClass("mobile-nav");
    $(this).toggleClass("is-active");
 });
+
+
+
+
+// $('.hover_me').mouseenter(function(){
+//    $('.hover_options').show(); 
+// });
+// $('.hover_me').mouseleave(function(){
+//    $('.hover_options').hide(); 
+// });
+
+
+$('.hover_me').hover(
+   function(){$('.hover_options').css('display', 'block')}
+ );
+
+ $('.navigation').hover(
+   function(){$('.hover_options').css('display', 'none')}
+ );
