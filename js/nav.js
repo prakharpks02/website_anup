84b340b7370a4a25f7edc6dce01e188c7ee72934
Anup1410 <anupr1410@gmail.com>
2020-06-03 00:59:44 +0530

    aa

--- js/nav.js
+++ js/nav.js
@@ -23,3 +23,20 @@ $('.hover_me').hover(
  $('.navigation').hover(
    function(){$('.hover_options').css('display', 'none')}
  );
+
+
+
+ 
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#auto_scroll').fadeIn(); 
+      } else { 
+          $('#auto_scroll').fadeOut(); 
+      } 
+  }); 
+  $('#auto_scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
